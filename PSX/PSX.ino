0637281c9ecffaa8854b1fe7f99a88e6bbab6ab4
MelisekVojtisek <grygavojta@gmail.com>
2023-06-13 22:12:14 +0200

    Update PSX.ino

--- PSX/PSX.ino
+++ PSX/PSX.ino
@@ -14,12 +14,33 @@ int pinS2 = 6;
 int pinS3 = 7;
 int pinSIG = A0;
 
+int rozsah[16][2]={
+  {0,4},   //0
+  {5,9},   //1
+  {10,14}, //2
+  {15,19}, //3
+  {20,24}, //4
+  {25,29}, //5
+  {30,34}, //6
+  {35,39}, //7
+  {40,44}, //8
+  {45,49}, //9
+  {50,54}, //10
+  {55,59}, //11
+  {60,64}, //12
+  {65,69}, //13
+  {70,74}, //14
+  {75,79}  //15
+};
+
 Adafruit_NeoPixel LEDterce = Adafruit_NeoPixel(80, LTerpin, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel LEDmag = Adafruit_NeoPixel(50, LMpin, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel LEDzlu = Adafruit_NeoPixel(50, LZpin, NEO_GRB + NEO_KHZ800);
 
 int skorem = 0;
 int skorez = 0;
+
+bool stav = 0;
 
 int Mterc;
 int Zterc;
@@ -77,25 +98,62 @@ void loop() {
     
     do {
 
-      Mterc = Rand(0,15);
-      Zterc = Rand(0,15);
+      Mterc = random(0,15);
+      Zterc = random(0,15);
       
-    } while (Mter != Zterc);
+    } while (Mterc != Zterc);
 
 
-    for(int i=x;i<y;i++){
+    for(int i = rozsah[Mterc][0]; i < rozsah[Mterc][1];i++){
 
       LEDterce.setPixelColor(i, LEDterce.Color(255,0,255)); 
 
       LEDterce.show(); 
     }
 
-    for(int i=x;i<y;i++){
+    for(int i = rozsah[Zterc][0]; i < rozsah[Zterc][1];i++){
 
       LEDterce.setPixelColor(i, LEDterce.Color(255,255,0)); 
 
       LEDterce.show(); 
     }
+
+    do{
+      if(nactiKanal(Mterc)>citlivost){
+        skorem =+ 5;
+
+        for(int i=0;i<=skorem;i++){
+
+          LEDmag.setPixelColor(i, LEDmag.Color(255,0,255)); 
+
+          LEDmag.show(); 
+        }
+
+        for(int i=0;i<6;i++){
+
+          for(int i = rozsah[Mterc][0]; i < rozsah[Mterc][1];i++){
+
+            LEDterce.setPixelColor(i, LEDterce.Color(0,0,0)); 
+
+            LEDterce.show(); 
+          }
+
+          delay(100);
+
+          for(int i = rozsah[Mterc][0]; i < rozsah[Mterc][1];i++){
+
+            LEDterce.setPixelColor(i, LEDterce.Color(255,0,255)); 
+
+            LEDterce.show(); 
+          }
+
+          delay(100);
+
+        }
+
+
+      };
+    } while();
 
   }
   else if (skorem > 50 && skorez < 50){
